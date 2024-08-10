72c748dcd06854c807d2ce747635add5f821f175
Gagan-Sky <gagankumar8294@gmail.com>
2024-08-10 17:21:53 +0530

    context-project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/Total.module.css";
+import { useContext } from 'react';
+import { itemContext } from "../itemContext";
+
+function Navbar() {
+  const value = useContext(itemContext);
+  console.log(value);
+  return (
+    <div className={styles.container}>
+      <h1>Total : &#x20B9; {value.total}</h1>
+      <h1>Items: {value.item}</h1>
+    </div>
+  );
+}
+
+export default Navbar;
